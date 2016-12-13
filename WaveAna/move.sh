9cfa82089ff4e719b16f44b8fce658383a5bca1d
wuchen <wuchen1106@gmail.com>
2016-12-14 06:13:57 +0900

    Added scripts

---
+++ WaveAna/move.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mv *.aa.pdf  *.aa.png  pics/aa
+mv *.at.pdf  *.at.png  pics/at
+mv *.st.pdf  *.st.png  pics/st
+mv *.sum.pdf *.sum.png pics/sum
+mv *.tdc.pdf *.tdc.png pics/tdc
+mv *.wf.pdf  *.wf.png  pics/wf
